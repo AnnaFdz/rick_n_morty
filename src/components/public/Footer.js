4797163fbc9c85060eb501d3a145b0075ea30956
Ana Fernandez <anny.fdz@gmail.com>
2024-07-02 17:19:51 -0300

    footer home-/ now home-homepage

--- src/components/public/Footer.js
+++ src/components/public/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
            
             <ul className="nav col-md-4 justify-content-end">   
             <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Top</a></li> 
-                <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Home</a></li> 
+                <li className="nav-item"><a href="homepage" className="nav-link px-2 text-muted">Home</a></li> 
                 
             </ul>
         </footer>
